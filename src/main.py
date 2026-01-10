165dcea58a1fd566d1886fbe48e9884bb1765849
Ryan Survilo <rcsurvilo@gmail.com>
2026-01-09 19:04:00 -0600

    copy static content to public

--- src/main.py
+++ src/main.py
@@ -1,7 +1,43 @@
+import os
+import shutil
+
 from textnode import TextType, TextNode
 
+
 def main():
-    text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(text_node)
+    src = "static"
+    dest = "public"
+
+    if not os.path.isdir(src):
+        print("src directory does not exist")
+        os._exit(1)
+    if not os.path.isdir(dest):
+        print("dest directory does not exist")
+        os._exit(1)
+
+    empty_dir(dest)
+    copy_dir(src, dest)
+
+def empty_dir(path_to_dir):
+    contents = os.listdir(path_to_dir)
+    for content in contents:
+        path = path_to_dir + "/" + content        
+        if os.path.isfile(path):
+            os.remove(path)
+        elif os.path.isdir(path):
+            shutil.rmtree(path)
+
+def copy_dir(src, dest):
+    contents = os.listdir(src)
+    for content in contents:
+        path = src + "/" + content        
+        if os.path.isfile(path):
+            shutil.copy(path, dest)
+        elif os.path.isdir(path):
+            joined_dest = os.path.join(dest, content)
+            os.mkdir(joined_dest)
+            copy_dir(path, joined_dest)
+
+
 
 main()
